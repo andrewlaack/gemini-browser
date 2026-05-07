8ce55a6cdc7a38d876b01bf5f1aef0068698e99b
Andrew Laack <andrew@laack.co>
2026-05-06 22:02:53 -0500

    Starting to understand the rendering stuff

--- main.go
+++ main.go
@@ -18,16 +18,60 @@ func main(){
 		panic(err)
 	}
 
+
 	bodyBytes, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+
 	text := string(bodyBytes)
 
+	// links := ParseLinks(text, os.Args[1])
 
 	app := tview.NewApplication()
 
 	textView := tview.NewTextView()
 	textView.SetBackgroundColor(tcell.ColorDefault)
-
 	textView.SetText(text)
+	
+	selectionMode := false
+
+	textView.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+			switch event.Rune() {
+			case ' ':
+				selectionMode = !selectionMode
+			case '1', '2','3','4','5','6','7','8','9','0':
+				if selectionMode {
+					selectionMode = false
+					go func() {
+						client := &gemini.Client{ConnectTimeout: 5 * time.Second}
+						resp, err := client.Fetch("gemini://blog.laack.co")
+
+						if err != nil {
+							panic(err)
+						}
+
+						bodyBytes, err := io.ReadAll(resp.Body)
+						if err != nil {
+							panic(err)
+						}
+
+						resp.Body.Close()
+
+						textNew := string(bodyBytes)
+
+						app.QueueUpdateDraw(func() {
+							textView.SetText(textNew)
+						})
+					}()
+
+					return nil
+				}
+			}
+
+			return event
+		})
+
+
+
 
 	if err := app.SetRoot(textView, true).SetFocus(textView).Run(); err != nil {
 		panic(err)
