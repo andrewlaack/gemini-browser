69af76a184c2c6dfd81ffd2562f6082c3baa10cb
Andrew Laack <andrew@laack.co>
2026-09-25 18:50:17 -0500

    Updated messaging prompt for redirect

--- src/main.cpp
+++ src/main.cpp
@@ -189,7 +189,7 @@ void draw(DrawState& ds) {
     }
 
     if (ds.handleRedirect) {
-        std::string toShow = "Redirect to " + ds.metaLine + " (y/n): ";
+        std::string toShow = "Redirect to \"" + ds.metaLine + "\" (y/n): ";
         drawInputBox(toShow, ds.redirInput);
     }
 
